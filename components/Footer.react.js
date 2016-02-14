53b93ad8186ee7f8f858f1fca713c7d60ab7b32d
DracoBlue <JanS@DracoBlue.de>
2016-02-14 15:27:58 +0100

    Fixed footer color

--- components/Footer.react.js
+++ components/Footer.react.js
@@ -6,7 +6,8 @@ module.exports = Footer = React.createClass({
   render: function(){
     return (
       <footer className={"show-" + this.props.tab}>
-            <a href="#top" className="timeline" onClick={this.props.onTimelineTab}>Timeline  ({this.props.timeline_count})</a> | <a href="#top" className="mentions" onClick={this.props.onMentionsTab}>Mentions ({this.props.mentions_count})</a> |  <a href="https://web.twtxt.org">web.twtxt.org</a> (<a href="https://github.com/DracoBlue/twtxt-ui">source</a>)
+            <a href="#top" className="tab timeline" onClick={this.props.onTimelineTab}>Timeline  ({this.props.timeline_count})</a> <a href="#top" className="tab mentions" onClick={this.props.onMentionsTab}>Mentions ({this.props.mentions_count})</a><br/>
+        Copyright 2016 by <a className="external-link" href="https://dracoblue.net">DracoBlue</a> (<a className="external-link" href="https://github.com/DracoBlue/twtxt-ui">source</a>)
       </footer>
     )
   }
