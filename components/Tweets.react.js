dbdd9ceb6e130c2381e5296168cbc649a7cb6cfd
DracoBlue <JanS@DracoBlue.de>
2016-03-18 21:55:18 +0100

    Added info, if nobody posted anything, yet.

--- components/Tweets.react.js
+++ components/Tweets.react.js
@@ -3,6 +3,7 @@
 var React = require('react');
 var Tweet = require('./Tweet.react.js');
 var ComposeTweet = require('./ComposeTweet.react.js');
+var moment = require('moment');
 
 module.exports = Tweets = React.createClass({
 
@@ -15,6 +16,22 @@ module.exports = Tweets = React.createClass({
         <Tweet key={"tweet-" + tweet.id} tweet={tweet} />
       )
     });
+
+    if (this.props.tweets.length == 0) {
+      var tweet = {
+        "author": "system",
+        "active": true,
+        "author_url": "http://web.twtxt.org/twtxt.txt",
+        timestamp: moment(),
+        displayTime: moment().format('YYYY/MM/DD HH:mm'),
+        "text": "You will see all tweets here, but nobody who you follow has posted anything, yet.",
+        "rawText": "You will see all tweets here, but nobody who you follow has posted anything, yet."
+      };
+
+      content = [(
+        <Tweet key={"tweet-no-tweet"} tweet={tweet} />
+      )];
+    }
 
     if (this.props.canPost) {
       content.unshift((<ComposeTweet onPostMessage={this.props.onPostMessage}/>));
